832f8681acf1919b7115adacb6388605ef4328d7
syahrulfahmi <syahrulfahmi331@gmail.com>
2025-11-24 22:19:40 +0700

    fix run mlflow

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,4 +1,5 @@
 import mlflow
+import dagshub
 import pandas as pd
 import tensorflow as tf
 import os
@@ -13,9 +14,12 @@ from tensorflow.keras.callbacks import EarlyStopping
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 
-# mlflow.set_experiment("Build Model with LSTM")
+# dagshub.init(repo_owner='syahrulfahmi', repo_name='mlsystem-submission-syahrul-fahmi', mlflow=True)
 
 if __name__ == "__main__":
+
+    mlflow.set_tracking_uri(os.getenv("MLFLOW_TRACKING_URI"))
+    mlflow.set_experiment("Build Model with LSTM")
 
     # load data
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
